a864cfd37f74fe40901705c0a55753ae4c88a3e5
Stuart McInnes <smcinnes1706@outlook.com>
2019-05-13 21:59:00 +0100

    Completed the module 4

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http'
 import { getLocaleNumberSymbol } from '@angular/common';
+import {Observable } from 'rxjs/Observable'
+import { Album } from './album';
+
 import 'rxjs/add/operator/map'
 @Injectable()
 export class ProductService {
@@ -12,7 +15,7 @@ export class ProductService {
   }
 
 
-  public getAlbum(id: number) {
-    return this._http.get(this._albumUrl).map((response) => response.json()) ;
+  public  getAlbum(id: number): Observable<Album> {
+    return this._http.get(this._albumUrl).map((response) => <Album>response.json()) ;
   }
 }
